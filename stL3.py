9dffe3b04f139755d7a7e22af07870a165086d60
Jean_D <jadelobelle@gmail.com>
2024-12-26 10:54:51 +0100

    Update stL3.py

--- stL3.py
+++ stL3.py
@@ -1,31 +1,9 @@
 import streamlit as st
-from streamlit_authenticator import Authenticate
 from streamlit_option_menu import option_menu
 
 
 
 st.write("Hello World")
-# Nos données utilisateurs doivent respecter ce format
-
-lesDonneesDesComptes = {'usernames': {'utilisateur': {'name': 'utilisateur',
-   'password': 'utilisateurMDP',
-   'email': 'utilisateur@gmail.com',
-   'failed_login_attemps': 0, # Sera géré automatiquement
-   'logged_in': False, # Sera géré automatiquement
-   'role': 'utilisateur'},
-  'root': {'name': 'root',
-   'password': 'rootMDP',
-   'email': 'admin@gmail.com',
-   'failed_login_attemps': 0, # Sera géré automatiquement
-   'logged_in': False, # Sera géré automatiquement
-   'role': 'administrateur'}}}
-
-authenticator = Authenticate(
-    lesDonneesDesComptes, # Les données des comptes
-    "cookie name", # Le nom du cookie, un str quelconque
-    "cookie key", # La clé du cookie, un str quelconque
-    30, # Le nombre de jours avant que le cookie expire 
-)
 
 import streamlit as st
 # Importation du module
