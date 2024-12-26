561a755a171c80bcdb5d9617c01c67933489d877
Jean_D <jadelobelle@gmail.com>
2024-12-26 11:23:31 +0100

    Update stL3.py

--- stL3.py
+++ stL3.py
@@ -1,24 +1,8 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
+
 
 
 
 st.write("Hello World")
 
-import streamlit as st
-# Importation du module
-from streamlit_option_menu import option_menu
 
-# Création du menu qui va afficher les choix qui se trouvent dans la variable options
-selection = option_menu(
-            menu_title=None,
-            options = ["Accueil", "Photos"]
-        )
-
-
-# On indique au programme quoi faire en fonction du choix
-if selection == "Accueil":
-    st.write("Bienvenue sur la page d'accueil !")
-elif selection == "Photos":
-    st.write("Bienvenue sur mon album photo")
-# ... et ainsi de suite pour les autres pages
